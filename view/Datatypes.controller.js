103d5afeebec5d4549d26a89c37bc9427b01688b
kicker123 <chriskne+00@gmail.com>
2015-10-03 12:44:54 +0000

    alignment with presentation

--- view/Datatypes.controller.js
+++ view/Datatypes.controller.js
@@ -2,23 +2,18 @@ jQuery.sap.require("sap.training.type.ZipCode");
 
 sap.ui.controller("sap.training.view.Datatypes", {
 
-	/**
-	 * Called when a controller is instantiated and its View controls (if available) are already created.
-	 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-	 * @memberOf sap.training.view.Main
-	 */
 	onInit: function() {
 
-		var oCore = sap.ui.getCore();
+		// var oCore = sap.ui.getCore();
 
-		oCore.attachParseError(this.onError);
+		// oCore.attachParseError(this.onError);
 
-		oCore.attachValidationError(this.onError);
+		// oCore.attachValidationError(this.onError);
 
-		oCore.attachValidationSuccess(function(oEvent) {
-			var oElement = oEvent.getParameter("element");
-			oElement.setValueState(sap.ui.core.ValueState.None);
-		});
+		// oCore.attachValidationSuccess(function(oEvent) {
+		// 	var oElement = oEvent.getParameter("element");
+		// 	oElement.setValueState(sap.ui.core.ValueState.None);
+		// });
 	},
 
 	onError: function(oEvent) {
@@ -29,32 +24,6 @@ sap.ui.controller("sap.training.view.Datatypes", {
 		sap.m.MessageToast.show(
 			oException.name + ": " + oException.message);
 	},
-
-	/**
-	 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-	 * (NOT before the first rendering! onInit() is used for that one!).
-	 * @memberOf sap.training.view.Main
-	 */
-	//	onBeforeRendering: function() {
-	//
-	//	},
-
-	/**
-	 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-	 * This hook is the same one that SAPUI5 controls get after being rendered.
-	 * @memberOf sap.training.view.Main
-	 */
-	//	onAfterRendering: function() {
-	//
-	//	},
-
-	/**
-	 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-	 * @memberOf sap.training.view.Main
-	 */
-	//	onExit: functon() {
-	//
-	//	}
 
 	onInputError: function(oEvent) {
 
@@ -73,5 +42,5 @@ sap.ui.controller("sap.training.view.Datatypes", {
 
 		var oTextView = this.getView().byId("myTextView");
 		oTextView.setText("");
-	},
+	}
 });
